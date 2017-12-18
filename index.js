e31bcf0e080fe283a9f4180a6ed04302be02628b
Caz <cazyw@users.noreply.github.com>
2017-12-18 16:54:40 +1100

    Add sequencing for both the computer and the player and error detection

--- index.js
+++ index.js
@@ -1,19 +1,107 @@
-let seq = [];
 const hov = document.getElementsByClassName('game')[0];
 console.log(hov);
+let seq = [];
+let playerSeq = [];
+let count = 0;
+let playerCount = 0;
+let playerTurn = false;
+let timeShow = 1000;
+let timeGap = 500;
 
-hov.addEventListener('mouseover', (e) => {
-  console.log(e.target.id);
-  let colorSelected = document.getElementById(e.target.id);
-  try {
+hov.addEventListener('mousedown', (e) => {
+  if (playerTurn) {
+    console.log('Player selects: ', e.target.id);
+    let colorSelected = document.getElementById(e.target.id);
     colorSelected.classList.add('light');
-  } catch(e){}
+    playerSeq.push(e.target.id);
+    console.log('seq: ', seq);
+    console.log('player seq: ', playerSeq);
+  }
 });
-hov.addEventListener('mouseout', (e) => {
-  console.log(e.target.id);
-  let colorSelected = document.getElementById(e.target.id);
-  try {
+hov.addEventListener('mouseup', (e) => {
+  if (playerTurn) {
+    console.log(e.target.id);
+    console.log('count ', count, 'playercount ', playerCount);
+    let colorSelected = document.getElementById(e.target.id);
     colorSelected.classList.remove('light');
-  } catch(e){}
+    if (!correctMatch(playerCount)) {
+      console.log(`failed at round ${playerCount+1}. Restart!`);
+      setTimeout(resetGame, 2000);
+    }
+    else if (playerSeq.length === seq.length){
+      console.log(`Made it through round ${count+1}. Next!`);
+      playerTurn = !playerTurn;
+      count++;
+      setTimeout(playRound, 2000);
+    } 
+    else {
+      playerCount++;
+    }
+  }
 });
 
+const generateRandom = () => {
+  return Math.floor(Math.random() * 4);
+}
+
+const addSeq = (button) => {
+  seq.push(`box${button}`);
+}
+
+const highlightSeq = (i) => {
+  let colorSelected = document.getElementById(seq[i]);
+  console.log('highlightSeq ', seq[i]);
+  if (i < seq.length){
+    colorSelected.classList.add('light');
+    setTimeout(function(){
+      colorSelected.classList.remove('light');
+      setTimeout(function() {
+        i++;
+        highlightSeq(i);
+      }, timeGap);
+    },timeShow);
+  } else {
+    playerPlays();
+  }
+}
+
+const resetGame = () => {
+  count = 0;
+  playerCount = 0;
+  playerTurn = false;
+  seq = [];
+  playerSeq = [];
+  clearTimeout();
+  playRound();
+}
+
+const correctMatch = (i) => {
+  console.log(`count ${i}, expect ${seq[i]}, actual ${playerSeq[i]} `);
+  return seq[i] === playerSeq[i];
+}
+
+const playerPlays = () => {
+  clearTimeout();
+  console.log('in playerPlays');
+  playerTurn = true;
+  playerCount = 0;
+}
+
+
+
+const playRound = () => {
+  console.log('Round ', count + 1, 'count ', count);
+  addSeq(generateRandom());
+  console.log(seq);
+  playerSeq = [];
+  playerTurn = false;
+  highlightSeq(0);
+}
+
+const game = () => {
+  console.log(count);
+  console.log('start game');
+  playRound();
+}
+
+setTimeout(game, 1000);
--- js/simonGame.js
+++ js/simonGame.js
@@ -70,24 +70,109 @@
 "use strict";
 
 
-var seq = [];
 var hov = document.getElementsByClassName('game')[0];
 console.log(hov);
+var seq = [];
+var playerSeq = [];
+var count = 0;
+var playerCount = 0;
+var playerTurn = false;
+var timeShow = 1000;
+var timeGap = 500;
 
-hov.addEventListener('mouseover', function (e) {
-  console.log(e.target.id);
-  var colorSelected = document.getElementById(e.target.id);
-  try {
+hov.addEventListener('mousedown', function (e) {
+  if (playerTurn) {
+    console.log('Player selects: ', e.target.id);
+    var colorSelected = document.getElementById(e.target.id);
     colorSelected.classList.add('light');
-  } catch (e) {}
+    playerSeq.push(e.target.id);
+    console.log('seq: ', seq);
+    console.log('player seq: ', playerSeq);
+  }
 });
-hov.addEventListener('mouseout', function (e) {
-  console.log(e.target.id);
-  var colorSelected = document.getElementById(e.target.id);
-  try {
+hov.addEventListener('mouseup', function (e) {
+  if (playerTurn) {
+    console.log(e.target.id);
+    console.log('count ', count, 'playercount ', playerCount);
+    var colorSelected = document.getElementById(e.target.id);
     colorSelected.classList.remove('light');
-  } catch (e) {}
+    if (!correctMatch(playerCount)) {
+      console.log('failed at round ' + (playerCount + 1) + '. Restart!');
+      setTimeout(resetGame, 2000);
+    } else if (playerSeq.length === seq.length) {
+      console.log('Made it through round ' + (count + 1) + '. Next!');
+      playerTurn = !playerTurn;
+      count++;
+      setTimeout(playRound, 2000);
+    } else {
+      playerCount++;
+    }
+  }
 });
+
+var generateRandom = function generateRandom() {
+  return Math.floor(Math.random() * 4);
+};
+
+var addSeq = function addSeq(button) {
+  seq.push('box' + button);
+};
+
+var highlightSeq = function highlightSeq(i) {
+  var colorSelected = document.getElementById(seq[i]);
+  console.log('highlightSeq ', seq[i]);
+  if (i < seq.length) {
+    colorSelected.classList.add('light');
+    setTimeout(function () {
+      colorSelected.classList.remove('light');
+      setTimeout(function () {
+        i++;
+        highlightSeq(i);
+      }, timeGap);
+    }, timeShow);
+  } else {
+    playerPlays();
+  }
+};
+
+var resetGame = function resetGame() {
+  count = 0;
+  playerCount = 0;
+  playerTurn = false;
+  seq = [];
+  playerSeq = [];
+  clearTimeout();
+  playRound();
+};
+
+var correctMatch = function correctMatch(i) {
+  console.log('count ' + i + ', expect ' + seq[i] + ', actual ' + playerSeq[i] + ' ');
+  return seq[i] === playerSeq[i];
+};
+
+var playerPlays = function playerPlays() {
+  clearTimeout();
+  console.log('in playerPlays');
+  playerTurn = true;
+  playerCount = 0;
+};
+
+var playRound = function playRound() {
+  console.log('Round ', count + 1, 'count ', count);
+  addSeq(generateRandom());
+  console.log(seq);
+  playerSeq = [];
+  playerTurn = false;
+  highlightSeq(0);
+};
+
+var game = function game() {
+  console.log(count);
+  console.log('start game');
+  playRound();
+};
+
+setTimeout(game, 1000);
 
 /***/ })
 /******/ ]);
