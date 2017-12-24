4c0f8c1d11e0d92d8459399d07b28cd5521720f9
Caz <cazyw@users.noreply.github.com>
2017-12-24 16:51:09 +1100

    Adding Web Audio API frequencies to controls

--- index.js
+++ index.js
@@ -15,12 +15,32 @@ let sound = [164.81, 220.00, 277.18, 329.63]
 
 var audioCtx =  new (window.AudioContext || window.webkitAudioContext)();
 
-var osc = audioCtx.createOscillator();
-osc.type = 'sine';
-osc.frequency.value = 164.81;
-osc.connect(audioCtx.destination);
-osc.start(0);
-osc.disconnect(audioCtx.destination);
+var osc0 = audioCtx.createOscillator();
+osc0.type = 'square';
+osc0.frequency.value = sound[0];
+osc0.start(0);
+
+var osc1 = audioCtx.createOscillator();
+osc1.type = 'square';
+osc1.frequency.value = sound[1];
+osc1.start(0);
+
+var osc2 = audioCtx.createOscillator();
+osc2.type = 'square';
+osc2.frequency.value = sound[2];
+osc2.start(0);
+
+var osc3 = audioCtx.createOscillator();
+osc3.type = 'square';
+osc3.frequency.value = sound[3];
+osc3.start(0);
+
+var error = audioCtx.createOscillator();
+error.type = 'square';
+error.frequency.value = sound[3];
+error.start(0);
+
+// osc.disconnect(audioCtx.destination);
 
 resetListener.addEventListener('click', (e) => {
   resetGame();
@@ -62,9 +82,7 @@ const down = (e) => {
       playerSeq.push(e.target.id);
       console.log('seq: ', seq);
       console.log('player seq: ', playerSeq);
-      let boxNo = Number(e.target.id[3]);
-      osc.frequency.value = sound[boxNo];
-      osc.connect(audioCtx.destination);
+      eval(`osc${Number(e.target.id[3])}`).connect(audioCtx.destination);
     } catch(e){}
   }
 
@@ -77,17 +95,15 @@ const up = (e) => {
     console.log('count ', count, 'playercount ', playerCount);
     let colorSelected = document.getElementById(e.target.id);
     try {
-      osc.disconnect(audioCtx.destination);
+      eval(`osc${Number(e.target.id[3])}`).disconnect(audioCtx.destination);
       colorSelected.classList.remove('light');
       if (!correctMatch(playerCount)) {
         let moveStatus = document.getElementsByClassName('controlPanel')[0];
         moveStatus.classList.add('wrong');
-
-        osc.frequency.value = 80;
-        osc.connect(audioCtx.destination);
+        error.connect(audioCtx.destination);
         setTimeout(() => {
           moveStatus.classList.remove('wrong');
-          osc.disconnect(audioCtx.destination);
+          error.disconnect(audioCtx.destination);
 
         }, 500);
         playerTurn = false;
@@ -152,11 +168,10 @@ const highlightSeq = (i) => {
   if (i < seq.length){
     colorSelected.classList.add('light');
     let boxNo = Number(seq[i][3]);
-    osc.frequency.value = sound[boxNo];
-    osc.connect(audioCtx.destination);
+    eval(`osc${boxNo}`).connect(audioCtx.destination);
     setTimeout(function(){
       colorSelected.classList.remove('light');
-      osc.disconnect(audioCtx.destination);
+      eval(`osc${boxNo}`).disconnect(audioCtx.destination);
       setTimeout(function() {
         i++;
         highlightSeq(i);
--- js/simonGame.js
+++ js/simonGame.js
@@ -87,12 +87,32 @@ var sound = [164.81, 220.00, 277.18, 329.63];
 
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-var osc = audioCtx.createOscillator();
-osc.type = 'sine';
-osc.frequency.value = 164.81;
-osc.connect(audioCtx.destination);
-osc.start(0);
-osc.disconnect(audioCtx.destination);
+var osc0 = audioCtx.createOscillator();
+osc0.type = 'square';
+osc0.frequency.value = sound[0];
+osc0.start(0);
+
+var osc1 = audioCtx.createOscillator();
+osc1.type = 'square';
+osc1.frequency.value = sound[1];
+osc1.start(0);
+
+var osc2 = audioCtx.createOscillator();
+osc2.type = 'square';
+osc2.frequency.value = sound[2];
+osc2.start(0);
+
+var osc3 = audioCtx.createOscillator();
+osc3.type = 'square';
+osc3.frequency.value = sound[3];
+osc3.start(0);
+
+var error = audioCtx.createOscillator();
+error.type = 'square';
+error.frequency.value = sound[3];
+error.start(0);
+
+// osc.disconnect(audioCtx.destination);
 
 resetListener.addEventListener('click', function (e) {
   resetGame();
@@ -133,9 +153,7 @@ var down = function down(e) {
       playerSeq.push(e.target.id);
       console.log('seq: ', seq);
       console.log('player seq: ', playerSeq);
-      var boxNo = Number(e.target.id[3]);
-      osc.frequency.value = sound[boxNo];
-      osc.connect(audioCtx.destination);
+      eval('osc' + Number(e.target.id[3])).connect(audioCtx.destination);
     } catch (e) {}
   }
 };
@@ -147,17 +165,15 @@ var up = function up(e) {
     console.log('count ', count, 'playercount ', playerCount);
     var colorSelected = document.getElementById(e.target.id);
     try {
-      osc.disconnect(audioCtx.destination);
+      eval('osc' + Number(e.target.id[3])).disconnect(audioCtx.destination);
       colorSelected.classList.remove('light');
       if (!correctMatch(playerCount)) {
         var moveStatus = document.getElementsByClassName('controlPanel')[0];
         moveStatus.classList.add('wrong');
-
-        osc.frequency.value = 80;
-        osc.connect(audioCtx.destination);
+        error.connect(audioCtx.destination);
         setTimeout(function () {
           moveStatus.classList.remove('wrong');
-          osc.disconnect(audioCtx.destination);
+          error.disconnect(audioCtx.destination);
         }, 500);
         playerTurn = false;
         if (strictMode) {
@@ -218,11 +234,10 @@ var highlightSeq = function highlightSeq(i) {
   if (i < seq.length) {
     colorSelected.classList.add('light');
     var boxNo = Number(seq[i][3]);
-    osc.frequency.value = sound[boxNo];
-    osc.connect(audioCtx.destination);
+    eval('osc' + boxNo).connect(audioCtx.destination);
     setTimeout(function () {
       colorSelected.classList.remove('light');
-      osc.disconnect(audioCtx.destination);
+      eval('osc' + boxNo).disconnect(audioCtx.destination);
       setTimeout(function () {
         i++;
         highlightSeq(i);
